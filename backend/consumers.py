0a3a842fda26f0076bc294de77710e20dc023434
Loc Nguyen <vinhloc30796@gmail.com>
2022-11-10 17:29:58 +0800

    Add more events to finish the flow

--- backend/consumers.py
+++ backend/consumers.py
@@ -26,7 +26,40 @@ def start_delivery(
     return state | { "status": "active" }
 
 
+def pickup_products(
+    state: Dict,
+    event: Event,
+) -> Dict:
+    data = json.loads(event.data)
+    new_budget = state["budget"] - int(data["purchase_price"]) * int(data["quantity"])
+    
+    return state | {
+        "budget": new_budget,
+        "purchase_price": int(data["purchase_price"]),
+        "quantity": int(data["quantity"]),
+        "status": "collected",
+    }
+
+
+def deliver_products(
+    state: Dict,
+    event: Event,
+) -> Dict:
+    data = json.loads(event.data)
+    new_budget = state["budget"] + int(data["sell_price"]) * int(data["quantity"])
+    new_quantity = state["quantity"] - int(data["quantity"])
+    
+    return state | {
+        "budget": new_budget,
+        "sell_price": int(data["sell_price"]),
+        "quantity": new_quantity,
+        "status": "completed",
+    }
+
+
 CONSUMERS = {
     "CREATE_DELIVERY": create_delivery,
     "START_DELIVERY": start_delivery,
+    "PICKUP_PRODUCTS": pickup_products,
+    "DELIVER_PRODUCTS": deliver_products,
 }
